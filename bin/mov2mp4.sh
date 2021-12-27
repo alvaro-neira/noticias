399eb7d371fbf81e224b8f19e5179c220357181a
Alvaro Neira <aneira@proactiveoffice.com>
2021-12-27 20:49:10 -0300

    From Mac mini

---
+++ bin/mov2mp4.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+THEPATH="/Users/aneira/Desktop"
+FFMPEG="/opt/homebrew/Cellar/ffmpeg/4.4.1_3/bin/ffmpeg"
+
+number=$(ps aux | grep -v grep | grep -ci ffmpeg)
+
+if [ "$number" -gt 0 ]
+    then
+        echo "ffmpeg already running";
+        exit 1;
+fi
+
+
+echo $THEPATH
+for FILE in "$THEPATH"/*.mov; do
+  echo "$FILE"
+  [ -e "$FILE" ] || continue
+  filename="${FILE##*/}"
+  extension="${filename##*.}"
+  prefilename="${filename%.*}"
+  echo "$prefilename"
+  echo "$extension"
+  echo ""
+  cd $THEPATH
+  ${FFMPEG} -noautorotate -i "$prefilename.$extension" -vf scale=640:360:flags=neighbor -y "$prefilename.mp4" && rm "$prefilename.$extension"
+  echo "done with file $prefilename.$extension"
+  exit 0
+done
---
+++ bin/scapture.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+NSECONDS=3540
+DATEFULL=$(date '+%Y_%m_%d')
+THEHOUR=$(date '+%H')
+THEHOUR=${THEHOUR#0}
+THEHOUR=$(expr "$THEHOUR" + 1)
+
+if [ "$THEHOUR" -lt 10 ]; then
+    FILENAME="/Users/aneira/Desktop/tv24horas_${DATEFULL}_0${THEHOUR}.mov"
+else
+    FILENAME="/Users/aneira/Desktop/tv24horas_${DATEFULL}_${THEHOUR}.mov"
+fi
+echo "$FILENAME"
+date
+echo $NSECONDS
+
+/usr/sbin/screencapture -D 2 -V $NSECONDS -x -a "$FILENAME"
