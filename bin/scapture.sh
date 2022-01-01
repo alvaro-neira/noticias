625680e957d0cfa83584f7ba503663d46126d610
Alvaro Neira <aneira@proactiveoffice.com>
2021-12-31 22:18:42 -0300

    fix scapture.sh

--- bin/scapture.sh
+++ bin/scapture.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
 
-FILENAME="/Users/aneira/Desktop/tv24horas"
+NSECONDS=3540
 
 THEYEAR=$(date '+%Y')
 THEMONTH=$(date '+%m')
+THEMONTH=${THEMONTH#0}
 THEDAY=$(date '+%d')
 THEHOUR=$(date '+%H')
-
 THEHOUR=${THEHOUR#0}
 THEHOUR=$(expr "$THEHOUR" + 1)
 
-THEMONTH=${THEMONTH#0}
-
+FILENAME="/Users/aneira/Desktop/tv24horas"
 if [ "$THEHOUR" -lt 10 ]; then
-  THEHOUR="0${THEHOUR}"
+    THEHOUR="0${THEHOUR}"
 fi
 if [ "$THEMONTH" -lt 10 ]; then
-  THEMONTH="0${THEMONTH}"
+    THEMONTH="0${THEMONTH}"
 fi
-
 FILENAME="${FILENAME}_${THEYEAR}_${THEMONTH}_${THEDAY}_${THEHOUR}.mov"
+echo ""
+echo "filename:"
 echo "$FILENAME"
 date
-
-echo "$NSECONDS"
+echo $NSECONDS
 
 #Main Display
 /usr/sbin/screencapture -m -V $NSECONDS -x -a "$FILENAME"
