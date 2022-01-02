c8da35a119806b07650e6d05b02686269df75765
Alvaro Neira <aneira@proactiveoffice.com>
2022-01-02 19:55:19 -0300

    working upload

--- bin/upload.py
+++ bin/upload.py
@@ -1,11 +1,15 @@
 #!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
+import sys
 
 from google_drive.gdrive_folder import GDriveFolder
 
+if len(sys.argv) != 2:
+    print(f"Usage: % upload.py <full filename>")
+    exit(-1)
 # gd = GDriveFolder("1AbWZDZI-4VHyKaCtoJolzy5QxQgylCRq")
 gd = GDriveFolder("1cdm7j1VkdIim6GtjZIon34vMq5ahrg3v")
 
 gd.oauth2_connect()
-gd.upload('/Users/aneira/noticias/small.mp4')
+gd.upload(sys.argv[1])
 
 print("done")
