ec142d0281294ab26537de5bf7a6fc08340be3bf
Alvaro Neira <aneira@proactiveoffice.com>
2022-01-03 19:21:07 -0300

    frame_extract2 for extracting fast

--- frame_extract.py
+++ frame_extract.py
@@ -4,15 +4,21 @@ capture = cv2.VideoCapture("data/small.mp4")
 if not capture.isOpened():
     print("error!")
 cv2.namedWindow("video", cv2.WINDOW_AUTOSIZE)
-cont = 0
+
+
+def process_frame(numpy_frame, counter):
+    cv2.imshow("video", numpy_frame)
+    if counter % 60 == 0:
+        cv2.imwrite("data/frame_{}.png".format(counter), numpy_frame, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
+
+count = 0
 while capture.grab():
-    retval, frame_color = capture.retrieve()
-    cv2.imshow("video", frame_color)
-    if cont % 60 == 0:
-        cv2.imwrite("frame_{}.png".format(cont), frame_color, [cv2.IMWRITE_PNG_COMPRESSION, 0])
-    key = cv2.waitKey(10)
+    _, frame_color = capture.retrieve()
+    process_frame(frame_color, count)
+    key = cv2.waitKey(1)
     if key == ord('q') or key == 27:
         break
-    cont += 1
+    count += 1
 capture.release()
 cv2.destroyAllWindows()
---
+++ frame_extract2.py
@@ -0,0 +1,44 @@
+# Took 3:40 minutes, 1 frame per second, 1-hour video, without display the video. 3540 frames.
+
+from imutils.video import FileVideoStream
+from imutils.video import FPS
+import numpy as np
+import imutils
+import time
+import cv2
+
+
+def process_frame(numpy_frame, counter):
+    if counter % 60 == 0:
+        cv2.imshow("video", numpy_frame)
+        cv2.imwrite("data/frame_{}.png".format(counter), numpy_frame, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
+
+# start the file video stream thread and allow the buffer to
+# start to fill
+print("[INFO] starting video file thread...")
+fvs = FileVideoStream('data/tv24horas_2022_01_03_15.mp4').start()
+time.sleep(1.0)
+
+# start the FPS timer
+fps = FPS().start()
+
+count = 0
+# loop over frames from the video file stream
+while fvs.more():
+    frame = fvs.read()
+    if frame is None:
+        break
+    process_frame(frame, count)
+    # There is no waitKey()
+    fps.update()
+    count = count + 1
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+fvs.stop()
