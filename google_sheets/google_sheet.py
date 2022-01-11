1e1ae0e8c44e9ccde4ce38655e37314d6e6dd6bc
Alvaro Neira <alvaroneirareyes@gmail.com>
2022-01-11 14:33:38 -0300

    getting tab by name

--- google_sheets/google_sheet.py
+++ google_sheets/google_sheet.py
@@ -12,13 +12,14 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/aneira/noticias
 # authorize the clientsheet
 gc = gspread.authorize(creds)
 
-worksheet = gc.open('copy_of_paula')
-sheet_instance = worksheet.get_worksheet(2)
+worksheet = gc.open('Copy of Etiquetado-Paula')
+sheet_instance = worksheet.worksheet("2021-12-13-21 (0.3)")
 print(sheet_instance)
 
 print(f"total columns={sheet_instance.col_count}")
 
-records_data = sheet_instance.get_all_records()
-
+# records_data = sheet_instance.get_all_records()
+#
 sheet_instance.update_cell(1, 1, "asd")
 sheet_instance.update_cell(2, 2, "bla")
+
