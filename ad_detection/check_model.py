1100de1635a72ce55743b5f66d64eba2016332a5
Alvaro Neira <alvaroneirareyes@gmail.com>
2022-01-14 00:15:44 -0300

    got csv to compare

--- ad_detection/check_model.py
+++ ad_detection/check_model.py
@@ -16,69 +16,10 @@ import matplotlib.pyplot as plt
 from torchvision import datasets, models, transforms
 import time
 import copy
+import csv
 
 random.seed(30)
-
-# """Se exportan los datos con que se entreno el modelo, dejar así porque estos datos tienen las clases."""
-#
-# shutil.copy("/content/drive/MyDrive/Taller Diplomado IA/Data Comerciales/processed/data.zip", "/content/data.zip")
-# shutil.copy("/content/drive/MyDrive/Taller Diplomado IA/Data Comerciales/processed/Comerciales.csv", "/content/Comerciales.csv")
-# !unzip data.zip
-#
-# """El dataset de los datos con que se entreno el modelo."""
-#
-# df = pd.read_csv('Comerciales.csv', header=0, sep=";")
-#
-# df
-#
-# classesFinal = df["Clase"].unique()
-# #classesFinal = [cl.replace(' ', '_') for cl in classes]
-# print(classesFinal)
-#
-# """Se necesita de una sola carpeta, aquella que es de test. Esta carpeta se dejará vacía a excepción de una imagen en cada carpeta de cada clase. (Si no se hace esto se generá un error después)"""
-#
-# try:
-#   os.mkdir('dataset')
-# except OSError:
-#   print ("No se pudo crear folder dataset")
-# else:
-#   print ("Se creó folder dataset")
-#
-#
-# try:
-#   os.mkdir('dataset/test')
-# except OSError:
-#   print ("No se pudo crear folder dataset")
-# else:
-#   print ("Se creó folder dataset")
-#
-#
-#
-# for cl in classesFinal:
-#
-#
-#   try:
-#     os.mkdir(os.path.join('dataset', "test", cl))
-#   except OSError:
-#     print (f"No se pudo crear folder test {cl}")
-#   else:
-#     print (f"Se creó folder test {cl}")
-#
-# dataset = dict()
-#
-# for index, cl in enumerate(classesFinal):
-#     grouped_data = df.groupby(by=["Clase"]).get_group(cl)["Nombre Final"].tolist()
-#     dataset[classesFinal[index]] = grouped_data
-#
-# for k,v in dataset.items():
-#     print(f'Class: {k}, Length: {len(v)}')
-#
-#     print(v)
-#
-
-#
-# shutil.copy("/content/drive/MyDrive/Taller Diplomado IA/Data Comerciales/resnet18_finetuned.pth", "/content/resnet18_finetuned.pth")
-#
+device = 'cpu'
 path_dataset = '/Users/aneira/noticias/ad_detection/dataset/'
 mypath = '/Users/aneira/noticias/ad_detection/data_test/'
 path_data = '/Users/aneira/noticias/ad_detection/data/'
@@ -93,13 +34,7 @@ test_dataset = torchvision.datasets.ImageFolder(path_dataset + 'test',
                                                                                   mean=[0.485, 0.456, 0.406],
                                                                                   std=[0.229, 0.224, 0.225])]))
 
-#
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True)
-
-#
-# class_names = test_dataset.classes
-#
-device = 'cpu'
 
 
 def test_model_one(model, criterion):
@@ -145,51 +80,22 @@ model_ft.load_state_dict(torch.load("/Users/aneira/noticias/ad_detection/resnet1
                                     map_location=torch.device('cpu')))
 criterion = nn.CrossEntropyLoss()
 
-# ret_class = evaluate_imagen(path_data + "2021_10_27_09_F5100.jpg", model_ft, criterion)
-# print(ret_class)
+df_test = pd.read_csv('/Users/aneira/noticias/ad_detection/resultados.csv')
+with open('/Users/aneira/noticias/ad_detection/resultados_local.csv', 'w', encoding='UTF8', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(['id', 'Nombre Final', 'Clase', 'result_red', 'correct', 'result_local'])
+    for index, row in df_test.iterrows():
+        print(row['Nombre Final'])
+        result_local = evaluate_imagen(mypath + row['Nombre Final'] + '.png', model_ft, criterion)
+        writer.writerow([index, row['Nombre Final'], row['Clase'], row['result_red'], row['correct'], result_local])
 
+# ls_img = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 #
-# evaluate_imagen("/content/drive/MyDrive/Taller Diplomado IA/Resultados/resultados_09/result_2021_11_26_22_frame_102600.png",model_ft,criterion)
-#
-# shutil.copy("/content/drive/MyDrive/Taller Diplomado IA/Data Comerciales/processed/Comerciales_test.csv", "/content/Comerciales_test_v2.csv")
-#
-# shutil.copy("/content/drive/MyDrive/Taller Diplomado IA/Data Comerciales/processed/data_test.zip", "/content/data_test.zip")
-# shutil.copy("/content/drive/MyDrive/Taller Diplomado IA/Data Comerciales/processed/Comerciales_test.csv", "/content/Comerciales_test.csv")
-# !unzip data_test.zip
-#
-# from os import listdir
-# from os.path import isfile, join
-
-ls_img = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-compare = []
-img_list = []
-for image in ls_img:
-    result = evaluate_imagen(mypath + image, model_ft, criterion)
-    print(f"{result} {mypath}{image}")
-    compare.append(result)
-    img_list.append(image[:-4])
-#
-# compare
-#
-# df_test = pd.read_csv('Comerciales_test.csv', header=0, sep=",")
-# df_red = pd.DataFrame()
-# df_red['result_red'] = compare
-# df_red['Nombre Final'] = img_list
-# df_test = pd.merge(df_test, df_red, how='left', on='Nombre Final')
-# df_test.head()
-#
-# print(len(df_test))
-#
-# conditions = [
-#     (df_test['Clase'] == 'Noticias') & (df_test['result_red'] == 'Noticias'),
-#     (df_test['Clase'] == 'Comercial') & (df_test['result_red'] == 'Comercial'),
-#     (df_test['Clase'] == 'Noticias') & (df_test['result_red'] == 'Comercial'),
-#     (df_test['Clase'] == 'Comercial') & (df_test['result_red'] == 'Noticias'),
-#     ]
-# choices = [1, 1, 0, 0]
-# df_test['correct'] = np.select(conditions, choices,)
-# print(df_test['correct'].sum())
-#
-# df_test.to_csv('resultados.csv')
-#
+# compare = []
+# img_list = []
+# for image in ls_img:
+#     result = evaluate_imagen(mypath + image, model_ft, criterion)
+#     print(f"{result} {mypath}{image}")
+#     compare.append(result)
+#     img_list.append(image[:-4])
+print('done')
